b04d1c7d87fe361ff4ad56cb020a0a5cf7978a03
maksymnl <adw0988245131@gmail.com>
2023-05-18 19:20:32 +0300

    для ментора

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+
+
+const inputEl = document.getElementById('search-box');
+const countryListEl = document.querySelector('country-list');
+const countryInfoEl = document.querySelector('country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+const r = fetch('https://restcountries.com/#endpoints-name/2')
+.then(Response =>{
+  return Response.json();
+})
+.then(flag => {
+  console.log(glag);
+});
+.catch(error => {
+  console.log(error);
+})
